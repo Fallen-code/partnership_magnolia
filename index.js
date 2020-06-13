62daf6871743cc293c4b3d9a81034405dced6020
unknown <giacodavide882@gmail.com>
2020-06-13 16:14:46 +0200

    le sos

--- index.js
+++ index.js
@@ -20,12 +20,23 @@ setInterval(() =>{
             if(args[i] !== '')
             {
                 let canale = bot.channels.cache.get(args[i])
-                if(canale) canale.send('ciao')
+                if(canale)
+                {
+                    let embed = new discord.MessageEmbed()
+                    .setAuthor('Magnolia roleplay | Developer bot: lǝƃuɐ uǝllɐɟ#9672',bot.user.displayAvatarURL())
+                    .setColor('FF0000')
+                    .setTitle(':point_right:  Entra in Magnolia Roleplay! :point_left:')
+                    .setURL('https://discord.gg/XqUwQqp')
+                    .setThumbnail(bot.user.displayAvatarURL())
+                    .setDescription(`Se sei un fan di Fairy Tail e del sano RP entra in **Magnolia Roleplay**!!
+                                    Cosa Offriamo https://discord.gg/XqUwQqp`)
+                    canale.send(embed+'\nhttps://discord.gg/XqUwQqp')
+                }
                 else console.log('errore per il canale '+args[i])
             }
         }
     }
-}, 2000)
+}, 600000)
 
 bot.on('message', message =>{
     if(message.author.bot) return;
