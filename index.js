2415309f6f1d775366c25669847ab0d4a0429fec
unknown <giacodavide882@gmail.com>
2020-06-13 17:19:59 +0200

    standby

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ setInterval(() =>{
                     .setImage('https://66.media.tumblr.com/fdc58b082c15c3335b5cb5b56a97aec5/tumblr_nsp67bvXic1t1ygmno1_500.gif')
                     .setTimestamp()
                     .setFooter('Cordiali saluti | Staff Magnolia Roleplay', bot.user.displayAvatarURL())
-                    canale.send(embed)
-                    canale.send('https://discord.gg/XqUwQqp')
+                    //canale.send(embed)
+                    //canale.send('https://discord.gg/XqUwQqp')
                 }
                 else console.log('errore per il canale '+args[i])
             }
